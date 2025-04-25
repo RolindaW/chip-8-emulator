4a60cf2c7fc96364f6d7efe3c031d9b7bb5bcdad
RolindaW <56414667+RolindaW@users.noreply.github.com>
2025-04-25 22:32:57 +0800

    profile real cpu cycle time

--- Chip8Emulator/Chip8Emulator/main.cpp
+++ Chip8Emulator/Chip8Emulator/main.cpp
@@ -1,15 +1,24 @@
 #include <string>
 #include <chrono>  // Timer
-#include <thread>  // Sleep
+//#include <thread>  // Sleep
+#include <iostream>
 
 #include "chip8_defs.h"
 #include "chip8_romloader.h"
 #include "chip8_rom.h"
 #include "chip8_emulator.h"
 
+void LogRealCpuHz(int cpuHz)
+{
+	std::cout << "Real CPU Hz: ";
+	std::cout << std::dec << cpuHz;
+	std::cout << "\n";
+}
+
 int main()
 {
 	std::string rom_filename = "C://workspace/chip-8-emulator/roms/test/test_opcode_mod.ch8";  // TODO: Get from program args; make relative - environment
+	rom_filename = "C://workspace/chip-8-emulator/roms/invaders.rom";
 
 	Chip8RomLoader rom_loader;
 	Chip8Rom rom = rom_loader.LoadFromFile(rom_filename);
@@ -27,6 +36,11 @@ int main()
 	const auto timer_interval = std::chrono::microseconds(1000000 / CHIP8_TIMER_HZ);
 	const auto frame_interval = std::chrono::microseconds(1000000 / CHIP8_FRAME_HZ);
 
+	// Profile real CPU Hz
+	int cpu_cycle_count = 0;
+	auto last_second = last_cpu;
+	const auto second_interval = std::chrono::microseconds(1000000);
+
 	while (true)  // TODO: exit on console or window close event
 	{
 		auto now = clock::now();
@@ -34,6 +48,7 @@ int main()
 		// CPU
 		if (now - last_cpu >= cpu_interval) {
 			emulator.StepCPU();
+			cpu_cycle_count++;
 			last_cpu = now;
 		}
 
@@ -54,8 +69,16 @@ int main()
 		// Warning! Input is - wrongly - being handled in renderer class for the moment
 
 		// Relieve CPU
+		// Warning! Emulator becomes very slow (50-75 Hz) whenever performing sleep (with almost any value - up to one nanosecond)
 		// TODO: Make deterministic
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+		// Profiler
+		if (now - last_second >= second_interval) {
+			LogRealCpuHz(cpu_cycle_count);
+			cpu_cycle_count = 0;
+			last_second = now;
+		}
 	}
 
 	return 0;
